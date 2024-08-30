c9352d8899526b36898bb6cb2926abe4631cef9c
joao@aluno.senai.br <joao@aluno.senai.br>
2024-08-30 13:48:32 -0300

    Develop toggle theme

--- front_end/src/app/header/header.component.ts
+++ front_end/src/app/header/header.component.ts
@@ -92,4 +92,3 @@ export class HeaderComponent implements OnInit {
     }
   }
 }
-
--- front_end/src/app/layouts/main/main.component.ts
+++ front_end/src/app/layouts/main/main.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa o CommonModule
 import { HeaderComponent } from '../../header/header.component';
 import { SidebarComponent } from '../../sidebar/sidebar.component';
 import { FooterComponent } from '../../footer/footer.component';
 
-
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  isDarkTheme: boolean = false; // Inicie com o tema claro por padrão
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+  }
+}
